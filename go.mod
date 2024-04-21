5ac31c562a991776417dd1e2c4b54990c477eb65
fgitonga <mfredgitonga@gmail.com>
2024-04-22 01:50:45 +0300

    func can print ascii art: line break functionality pending

---
+++ go.mod
@@ -0,0 +1,13 @@
+module ascii
+
+go 1.22.1
+
+require github.com/salsaflow/salsaflow v0.20.0
+
+require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/shiena/ansicolor v0.0.0-20230509054315-a9deabde6e02 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- main/main.go
+++ main/main.go
@@ -5,30 +5,89 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	// ascii.Printer()
-	// ascii.PrintGrimReaper("Hello")
-	// opening a file
+	arg := os.Args[1:]
+
+	if len(arg) < 1 {
+		fmt.Println("Error : offer ")
+	}
+	
+	text := arg[0]
+	text2 := strings.Split(text, "\\n")
+	text3:= strings.Join(text2, "\n")
+ 	firstText := text3[:6]
+	//secondText := text3[1]
+	fmt.Println(text2)
+	fmt.Println(text3)
+	fmt.Println(string(firstText))
+	
+	groups := getText()
+
+	line1 := ""
+	line2 := ""
+	line3 := ""
+	line4 := ""
+	line5 := ""
+	line6 := ""
+	line7 := ""
+	line8 := ""
+	for letter := range firstText {
+		key := int(letter)
+		if lines, ok := groups[key]; ok {
+			if len(lines) > 0 {
+				line1 +=lines[0]
+				line2 +=lines[1]
+				line3 +=lines[2]
+				line4 +=lines[3]
+				line5 +=lines[4]
+				line6 +=lines[5]
+				line7 +=lines[6]
+				line8 +=lines[7]
+			}
+		}
+	}
+		fmt.Println(line1)
+		fmt.Println(line2)
+		fmt.Println(line3)
+		fmt.Println(line4)
+		fmt.Println(line5)
+		fmt.Println(line6)
+		fmt.Println(line7)
+		fmt.Println(line8)
+}
+
+func getText() map[int][]string {
 	file, err := os.Open("standard.txt")
 	if err != nil {
 		fmt.Println("Error :", err)
 	}
 	defer file.Close()
-	// read the data into a slice of bytes
-	/*
-		data := make([]byte, 1000) // initialises a slice of type byte, and length 1000.
-		count, err := file.Read((data))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("read %d bytes: %q\n", count, data[:count])
-	*/
 
+	var groupNumber int = 31
+	lineCount := 0
+	groups := make(map[int][]string)
+	// creates a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-	for i := 0; i<20 && scanner.Scan(); i++ {
-		fmt.Println(scanner.Text())
+		// skip  empty lines
+		if line == "" {
+			continue
+		}
+
+		if lineCount%8 == 0 {
+			groupNumber++
+		}
+		groups[groupNumber] = append(groups[groupNumber], line)
+		lineCount++
 	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
+	return groups
 }
